0576fe150a8b94d03d3067017dc80ef98a9981ae
Cokops <gerasev-z@mail.ru>
2025-12-12 22:37:29 +0300

    Work is Done

---
+++ Zad1/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+import uvicorn
+
+app = FastAPI()
+# Получаем абсолютный путь к директории со скриптом
+BASE_DIR = Path(__file__).parent
+# Указываем абсолютный путь к папке templates
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+languages = [
+    {"id": 1, "name": "Русский", "hello": "Привет"},
+    {"id": 2, "name": "Английский", "hello": "Hello"},
+    {"id": 3, "name": "Испанский", "hello": "Hola"},
+    {"id": 4, "name": "Французский", "hello": "Bonjour"},
+    {"id": 5, "name": "Немецкий", "hello": "Hallo"},
+]
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "langs": languages
+        }
+    )
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
+
+
+
+
+
---
+++ Zad2/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+tasks = [
+    {"id": 1, "title": "Купить молоко", "done": False},
+    {"id": 2, "title": "Сделать уроки", "done": True},
+]
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.post("/tasks")
+def add_task(title: str):
+    new_id = len(tasks) + 1
+    task = {"id": new_id, "title": title, "done": False}
+    tasks.append(task)
+    return task
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    return HTMLResponse(status_code=404,content="Не найдено!")
+
+@app.put("/tasks/{task_id}/toggle")
+def toggle_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["done"] = not task["done"]
+            return task
+    return HTMLResponse(status_code=404,content="Не найдено!")
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    global tasks
+    tasks = [t for t in tasks if t["id"] != task_id]
+    return {"deleted": task_id, "remaining": len(tasks)}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",reload=True)
